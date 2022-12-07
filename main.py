2f96ac01d2352a8c255f6ad01d8f77bec3e99635
V1ilze <120003886+V1ilze@users.noreply.github.com>
2022-12-07 17:00:33 +1000

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+g = int(input("Введите 1-ое число:"))
+x = int(input("Введите 2-ое число:"))
+sign = input("Введите знак операции:")
+if sign == "+":
+  res = x + g
+elif sign == "-":
+  res = g - x
+elif sign == "/":
+  res = g / x
+elif sign == "*":
+  res = g * x
+elif sign == "**":
+  res = g ** x
+else:
+  print("vsjbdrsjhdfjgs")
+
+
+print("Ответ =", res)
